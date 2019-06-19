027c985b0000b2f4b9d0c8c2b03a74edbd01d15d
Tsai, Tsai-Chieh <jecica196@gmail.com>
2019-06-19 09:46:35 +0800

    no message

--- week-2/js/app.js
+++ week-2/js/app.js
@@ -1,11 +1,15 @@
 const banner = document.getElementsByClassName('banner')[0];
 var count_banner_click = 0;
+
 const main_nav = document.getElementsByClassName('main-nav')[0];
 const close_menu = document.getElementsByClassName('close-menu')[0];
 const menu_btn = document.getElementsByClassName('menu-btn')[0];
 const display_mask = document.getElementsByClassName('display-mask')[0];
 var window_size = window.matchMedia("(min-width: 800px)");
 
+const btn = document.getElementsByClassName('btn')[0];
+var count_btn_click = 0;
+const row_2 = document.getElementsByClassName('row-2')[0];
 /* 
 // Request 1: Click to Change Text 
 // When user click on "Welcome Message" block, change text to "Have a Good Time!".
@@ -38,5 +42,24 @@ function close_display_mask(window_size) {
       display_mask.style.display = 'none';
   }
 }
-  close_display_mask(window_size) // Call listener function at run time
-  window_size.addListener(close_display_mask) // Attach listener function on state changes
+close_display_mask(window_size); // Call listener function at run time
+window_size.addListener(close_display_mask); // Attach listener function on state changes
+
+/* 
+// Request 3: Click to Show More Content Boxes.
+// There are some more content boxes waiting to show. When user clicks the 
+// Call-to-Action button, show those hidden content boxes.
+*/
+row_2.style.display = 'none';
+
+btn.addEventListener('click',() => {
+    count_btn_click ++;
+    if (count_btn_click % 2 == 1) {
+        btn.textContent = 'Close Action';
+        row_2.style.display = 'block';
+    }
+    else {
+        btn.textContent = 'Call to Action';
+        row_2.style.display = 'none';
+    }
+}) 
